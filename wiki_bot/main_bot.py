55b8b7955bb000f6705b13ec45f551a4054461db
Valeriy Batalov <59008622+Valeri017@users.noreply.github.com>
2022-07-21 19:25:55 +0500

    Add files via upload

---
+++ wiki_bot/main_bot.py
@@ -0,0 +1,102 @@
+import telebot
+import wikipedia, re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+wikipedia.set_lang("ru")
+bot = telebot.TeleBot('5388382515:AAGWCSXx6ZLiYdjIpYTuCtLMlhwm9yYRQyI')
+
+def clean_string(string):
+    string = string.lower()
+    r = [i for i in string if i in alphabet]
+    return ''.join(r)
+
+alphabet = ' 1234567890-йцукенгшщзхъфывапролджэячсмитьбюqwertyuiopasdfghjklzxcvbnm'
+
+def update():
+    with open('dialogues.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    blocks = content.split('\n')
+    dataset = []
+
+    for block in blocks:
+        replicas = block.split('\\')[:2]
+
+        if len(replicas) == 2:
+            pair = [clean_string(replicas[0]), clean_string(replicas[1])]
+            if pair[0] and pair[1]:
+                dataset.append(pair)
+                with open(r'request.txt', 'w') as file:
+                    for line in dataset:
+                        file.write(line + '\n')
+    X_text = []
+    y = []
+
+    for question, answer in dataset[:1000]:
+        X_text.append(question)
+        y += [answer]
+
+    global vectorizer
+    vectorizer = CountVectorizer()
+    X = victorizer.fit_transform(X_text)
+
+    global clf
+    clf = LogisticRegression()
+    clf.fit(X, y)
+
+update()
+
+def get_generative_replica(text):
+    text_vector = vectorizer.transform([text]).toarray()[0]
+    question = clf.predict([text_vextor])[0]
+    return question
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if(len((x.strip()))>3):
+                    wikitext2 = wikitext2 + x +'.'
+            else:
+                break
+        wikitext2 = re.sub('\([^()]*\)','',wikitext2)
+        wikitext2 = re.sub('\{[^{\}]*\)','',wikitext2)
+        return wikitext2
+    except Exception as e:
+        return 'В Википедии нет информации об этом'
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Здравствуйте")
+
+question = ""
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    command = message.text.lower()
+    if command =="не так":
+        bot.send_message(message.from_user.id, "a как?")
+        bot.register_next_step_handler(message, wrong)
+    else:
+        global question
+        question = command
+        reply = get_generative_replica(command)
+        if reply =="вики ":
+            bot.send_message(message.from_user.id, getwiki(command))
+        else:
+            bot.send_message(message.from_user.id, reply)
+
+def wrong(message):
+    a = f"{question}\{message.text.lower()} \n"
+    with open('dialogues.txt', "a", encoding='utf-8') as f:
+        f.write(a)
+    bot.send_message(message.from_user.id, "Готово")
+    update()
+bot.polling(none_stop=True)
